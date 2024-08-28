ac8ee9e58490cee76fceb0dabe05be9766c80414
swiftadmin <swiftdeveloperr@gmail.com>
2024-08-28 14:20:56 +0300

    prisma eklendi

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import AddTodo from "@/app/components/shared/AddTodo";
 import Todo from "@/app/components/shared/Todo";
+import React from 'react';
 
-
-
+// PrismaClient örneği
 const prisma = new PrismaClient();
+
+// Veri getirme fonksiyonu
 async function getData() {
     const data = await prisma.todo.findMany({
         select: {
@@ -19,26 +21,41 @@ async function getData() {
 
     return data;
 }
-const Home = async () => {
-    const data = await getData();
+
+// Todo tipi tanımlaması
+interface TodoType {
+    id: string;
+    title: string;
+    isCompleted: boolean;
+}
+
+// `getServerSideProps` fonksiyonunu kullanarak veriyi alıyoruz
+export async function getServerSideProps() {
+    const data: TodoType[] = await getData();
+
+    return {
+        props: {
+            data,
+        },
+    };
+}
+
+// Home bileşeni
+const Home: React.FC<{ data: TodoType[] }> = ({ data }) => {
     return (
         <div className="w-screen py-20 flex justify-center flex-col items-center">
-      <span className="text-3xl font-extrabold uppercase">
-        To-do-app
-      </span>
-            <h1 className=" text-3xl font-extrabold uppercase mb-5">
+            <span className="text-3xl font-extrabold uppercase">To-do-app</span>
+            <h1 className="text-3xl font-extrabold uppercase mb-5">
                 Next.js 14
-                <span className="text-orange-700 ml-2">
-          Server Actions
-        </span>
+                <span className="text-orange-700 ml-2">Server Actions</span>
             </h1>
 
-            <div className="flex justify-center flex-col items-center w-[1000px] ">
+            <div className="flex justify-center flex-col items-center w-[1000px]">
                 <AddTodo />
-                <div className=" flex flex-col gap-5 items-center justify-center mt-10 w-full">
-                    {data.map((todo,id) => (
-                        <div className={"w-full"} key={id}>
-                            <Todo todo={todo}/>
+                <div className="flex flex-col gap-5 items-center justify-center mt-10 w-full">
+                    {data.map((todo) => (
+                        <div className="w-full" key={todo.id}>
+                            <Todo todo={todo} />
                         </div>
                     ))}
                 </div>
